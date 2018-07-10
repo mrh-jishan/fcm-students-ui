7e4b2a54c71976dce7efd4d37111a864d3236af3
MD ROBIUL HASSAN <robiul.hassan@finterra.org>
2018-07-10 14:42:07 +0800

    Add INIT

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def hello_name():
+    return render_template('index.html', name='index page')
+
+
+@app.route('/')
+def index():
+    return redirect("/index", code=302)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error-404.html', data=str(e)), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template('error-404.html', data=str(e)), 405
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
