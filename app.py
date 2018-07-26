486f23f1be6e0a6e0ca811526bce6ffe9feb01f8
MD ROBIUL HASSAN <robiul.hassan@finterra.org>
2018-07-26 21:41:16 +0800

    update

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import os
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect, jsonify, request
 import pandas as pd
 import lib
 
 app = Flask(__name__)
-
-
-@app.route('/index')
-def index():
-    return render_template('index.html', name='index page')
+app.debug = True
 
 
 @app.route('/')
 def root():
     return redirect("/index"), 302
+
+
+@app.route('/index', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST' and request.files:
+        f = request.files['fileupload']
+        df = pd.read_csv(f.stream)
+        return render_template('index.html',
+                               name=dict({'nodeKeyProperty': 'id', 'nodeDataArray': lib.getNodeDataArray(df),
+                                          'linkDataArray': lib.getLinkDataArray(df)}))
+    return render_template('index.html', name='index page')
 
 
 @app.route('/get_data')
@@ -31,8 +38,6 @@ def page_not_found(e):
 @app.errorhandler(405)
 def method_not_allowed(e):
     return render_template('error-404.html', data=str(e)), 405
-
-
 
 
 if __name__ == "__main__":
