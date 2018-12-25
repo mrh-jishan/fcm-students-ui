c7e3d2953accf79774c820b1750aa5c3308de0cd
Robin Hassan <ra.onerobin@gmail.com>
2018-12-26 04:01:41 +0800

    Update on click table view

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,19 @@
 // get the data
-drawFCM = function (filterText) {
+drawFCM = (filterText) => {
     d3.csv("static/file/output.csv", function (error, links) {
         var nodes = {};
 
-        links = links.filter(function (res) {
-            return res.source == filterText || res.target == filterText;
-        })
+        links = links.filter((res) => res.source == filterText || res.target == filterText)
 
         // Compute the distinct nodes from the links.
         links.forEach(function (link) {
-            link.source = nodes[link.source] ||
-                (nodes[link.source] = { name: link.source });
-            link.target = nodes[link.target] ||
-                (nodes[link.target] = { name: link.target });
+            link.source = nodes[link.source] || (nodes[link.source] = { name: link.source });
+            link.target = nodes[link.target] || (nodes[link.target] = { name: link.target });
             link.value = +link.value;
         });
 
-        var width = window.innerWidth,
-            height = window.innerHeight,
+        var width = $("#container").innerWidth(),
+            height = window.innerHeight-100,
             color = d3.scale.category20c();
 
 
@@ -30,6 +26,7 @@ drawFCM = function (filterText) {
             .on("tick", tick)
             .start();
 
+
         // Set the range
         var v = d3.scale.linear().range([0, 400]);
 
@@ -38,23 +35,26 @@ drawFCM = function (filterText) {
 
         // asign a type per value to encode opacity
         links.forEach(function (link) {
-            if (v(link.value) <= 25) {
+            if (v(link.value) <= 0.1) {
                 link.type = "twofive";
-            } else if (v(link.value) <= 50 && v(link.value) > 25) {
+            } else if (v(link.value) <= 0.1 && v(link.value) > 0.2) {
                 link.type = "fivezero";
-            } else if (v(link.value) <= 75 && v(link.value) > 50) {
+            } else if (v(link.value) <= 0.2 && v(link.value) > 0.3) {
                 link.type = "sevenfive";
-            } else if (v(link.value) <= 100 && v(link.value) > 75) {
+            } else if (v(link.value) <= 0.3 && v(link.value) > 0.4) {
                 link.type = "onezerozero";
             }
         });
 
-        var svg = d3.select("#container").append("svg")
+        var svg = d3.select("#container")
+            .append("svg")
             .attr("width", width)
             .attr("height", height);
 
+
         // build the arrow.
-        svg.append("svg:defs").selectAll("marker")
+        svg.append("svg:defs")
+            .selectAll("marker")
             .data(["end"])      // Different link/path types can be defined here
             .enter().append("svg:marker")    // This section adds in the arrows
             .attr("id", String)
@@ -69,16 +69,15 @@ drawFCM = function (filterText) {
 
 
 
-
         // add path
         // add the links and the arrows
-        var path = svg.append("svg:g").selectAll("path")
+        var path = svg.append("svg:g")
+            .selectAll("path")
             .data(force.links())
-            .enter().append("svg:path")
-            .attr("class", function (d) { return "link " + d.type; })
-            .attr("id", function (d) {
-                return "invis_" + d.source.index + "-" + d.value + "-" + d.target.index;
-            })
+            .enter()
+            .append("svg:path")
+            .attr("class", (d) => { return "link " + d.type; })
+            .attr("id", (d) => "invis_" + d.source.index + "-" + d.value + "-" + d.target.index)
             .attr("marker-end", "url(#end)");
 
 
@@ -86,12 +85,14 @@ drawFCM = function (filterText) {
         var pathLabel = svg.selectAll(".pathLabel")
             .data(force.links());
 
-        pathLabel.enter().append("g").append("svg:text")
+        pathLabel.enter()
+            .append("g")
+            .append("svg:text")
             .attr("class", "pathLabel")
             .append("svg:textPath")
             .attr("startOffset", "50%")
             .attr("text-anchor", "middle")
-            .attr("xlink:href", function (d) { return "#invis_" + d.source.index + "-" + d.value + "-" + d.target.index; })
+            .attr("xlink:href", (d) => { return "#invis_" + d.source.index + "-" + d.value + "-" + d.target.index; })
             .style("fill", "#000000")
             .style("font-size", 15)
             .text(function (d) { return d.value; });
@@ -120,11 +121,13 @@ drawFCM = function (filterText) {
             .attr("dy", ".35em")
             .text(function (d) { return d.name; });
 
+
+        // var g = svg.append("g")
+        //     .attr("class", "everything");
+
         // add the curvy lines
         function tick() {
-
             // d3.select("#Graph svg").selectAll("path")
-
             path.attr("d", function (d) {
                 var dx = d.target.x - d.source.x,
                     dy = d.target.y - d.source.y,
@@ -136,33 +139,49 @@ drawFCM = function (filterText) {
                     d.target.x + "," +
                     d.target.y;
             });
-
-
-
             node.attr("transform", function (d) {
                 return "translate(" + d.x + "," + d.y + ")";
             });
         }
 
         // action to take on mouse click
-        function click() {
-            d3.select(this).select("text").transition()
+        function click(d) {
+            d3.select(this).select("text")
+                .transition()
                 .duration(750)
                 .attr("x", 22)
                 .style("stroke", "lightsteelblue")
                 .style("stroke-width", ".5px")
                 .style("font", "20px sans-serif");
-            d3.select(this).select("circle").transition()
-                .duration(750)
-                .attr("r", 16);
+
+
+            // + res.outputstudents.find(x => x.target == d.target).value + 
+
+            $html = '';
+            links.filter((e) => e.source.name === d.name || e.target.name === d.name).forEach((res, index) => {
+                console.log(res);
+                $html += `<tr>
+                    <td>${res.source.name}</td>
+                    <td>${res.target.name}</td>
+                    <td>${res.value}</td>
+                    <td>${getExpertsValue(res)}</td>
+                </tr >`;
+            });
+            $("#selectedNode tbody").html($html);
+
+            // d3.select(this).select("circle").transition()
+            //     .duration(750)
+            //     .attr("r", 16);
         }
 
         // action to take on mouse double click
         function dblclick() {
-            d3.select(this).select("circle").transition()
+            d3.select(this).select("circle")
+                .transition()
                 .duration(750)
                 .attr("r", 6);
-            d3.select(this).select("text").transition()
+            d3.select(this).select("text")
+                .transition()
                 .duration(750)
                 .attr("x", 12)
                 .style("stroke", "none")
@@ -175,10 +194,9 @@ drawFCM = function (filterText) {
 }
 
 // get the data
-draw_fcm_for_students = function () {
+draw_fcm_for_students = () => {
     d3.csv("static/file/outputstudents.csv", function (error, links) {
         var nodes = {};
-
         // Compute the distinct nodes from the links.
         links.forEach(function (link) {
             link.source = nodes[link.source] ||
@@ -380,3 +398,11 @@ $getJSON = function (outputstudents, output) {
 }
 
 
+
+getExpertsValue = (data) => {
+    $getOutputData('static/file/output.csv', data.source).then(res => {
+        console.log(res);
+    })
+
+    return 'robin';
+}
